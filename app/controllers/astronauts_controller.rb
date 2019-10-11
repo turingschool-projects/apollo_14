dc057b952f6f2a82b10ab1f6360e51bad210fc75
Graham Thompson <grwthomps@gmail.com>
2019-10-11 10:22:16 -0600

    Create average_age method in astronauts controller

--- app/controllers/astronauts_controller.rb
+++ app/controllers/astronauts_controller.rb
@@ -2,4 +2,9 @@ class AstronautsController < ApplicationController
   def index
     @astronauts = Astronaut.all
   end
+
+  def average_age
+    binding.pry
+    average_age = (@astronauts.sum {|astronaut| astronaut.age}) / @astronauts.count
+  end
 end
