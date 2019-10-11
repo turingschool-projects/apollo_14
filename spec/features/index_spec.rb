9c792cfd7b3288155407d9e7f692da4b7fcbe29d
Ryan Hantak <rhantak@gmail.com>
2019-10-11 09:37:23 -0600

    Add index page to display all astronauts in database

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  describe "When I navigate to the index page" do
+    before(:each) do
+      @neil = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+      @buzz = Astronaut.create(name: "Buzz Aldrin", age: 34, job: "Lunar Module Pilot")
+      @apollo = Mission.create(title: "Apollo 11", time_in_space: 8)
+    end
+
+    it "I see a list of all astronauts' names, ages, and jobs" do
+
+      visit '/astronauts'
+      save_and_open_page
+
+      within("#astronaut-#{@neil.id}") do
+        expect(page).to have_content(@neil.name)
+        expect(page).to have_content(@neil.age)
+        expect(page).to have_content(@neil.job)
+
+        expect(page).to_not have_content(@buzz.name)
+      end
+
+      within("#astronaut-#{@buzz.id}") do
+        expect(page).to have_content(@buzz.name)
+        expect(page).to have_content(@buzz.age)
+        expect(page).to have_content(@buzz.job)
+
+        expect(page).to_not have_content(@neil.name)
+      end
+    end
+
+
+
+  end
+end
