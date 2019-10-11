1c0f598f2c54c7187465d7e4e082d987a0718b5f
Graham Thompson <grwthomps@gmail.com>
2019-10-11 10:21:56 -0600

    Create test for showing average age of all astronauts

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -1,27 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "astronauts show page" do
+  before(:each) do
+    @astronaut_1 = Astronaut.create(name: "John Spackleton", age: 42, job: "Commander")
+    @astronaut_2 = Astronaut.create(name: "Bob Johnson", age: 35, job: "Pilot")
+    @astronaut_3 = Astronaut.create(name: "Michael Schwartz", age: 37, job: "Flight Controller")
+  end
   it "can show a list of astronauts with attributs" do
-    astronaut_1 = Astronaut.create(name: "John Spackleton", age: 42, job: "Commander")
-    astronaut_2 = Astronaut.create(name: "Bob Johnson", age: 35, job: "Pilot")
-    astronaut_3 = Astronaut.create(name: "Michael Schwartz", age: 37, job: "Flight Controller")
-
     visit '/astronauts'
 
-    within "#astronaut-#{astronaut_1.id}" do
-      expect(page).to have_content(astronaut_1.name)
-      expect(page).to have_content(astronaut_1.age)
-      expect(page).to have_content(astronaut_1.job)
+    within "#astronaut-#{@astronaut_1.id}" do
+      expect(page).to have_content(@astronaut_1.name)
+      expect(page).to have_content(@astronaut_1.age)
+      expect(page).to have_content(@astronaut_1.job)
 
-      expect(page).to_not have_content(astronaut_2.name)
+      expect(page).to_not have_content(@astronaut_2.name)
     end
 
-    within "#astronaut-#{astronaut_2.id}" do
-      expect(page).to have_content(astronaut_2.name)
-      expect(page).to have_content(astronaut_2.age)
-      expect(page).to have_content(astronaut_2.job)
+    within "#astronaut-#{@astronaut_2.id}" do
+      expect(page).to have_content(@astronaut_2.name)
+      expect(page).to have_content(@astronaut_2.age)
+      expect(page).to have_content(@astronaut_2.job)
 
-      expect(page).to_not have_content(astronaut_1.name)
+      expect(page).to_not have_content(@astronaut_1.name)
     end
+  end
+
+  it "can show average age of all astronauts" do
+    visit '/astronauts'
+
+    expect(page).to have_content('Average Age: 38')
   end
 end
