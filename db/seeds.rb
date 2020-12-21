2d1d9be863f32cacc12de4746a1c3903a79dcc96
Ely Hess <ely.hess@me.com>
2020-12-21 10:35:52 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+astro1 = Astronaut.create!(name: "Bob", age: 34, job: "Commander")
+astro2 = Astronaut.create!(name: "Jill", age: 34, job: "Private")
+astro3 = Astronaut.create!(name: "Joe", age: 34, job: "Colonel")
+astro4 = Astronaut.create!(name: "Has", age: 34, job: "Lieutenant")
+
+mission1 = Mission.create!(title: "Mission to Mars", time_in_space: 20)
+mission2 = Mission.create!(title: "Mission to Moon", time_in_space: 1)
+mission3 = Mission.create!(title: "Mission to Sun", time_in_space: 80)
+
+AstronautMission.create!(astronaut: astro1, mission: mission1)
+AstronautMission.create!(astronaut: astro1, mission: mission2)
+AstronautMission.create!(astronaut: astro2, mission: mission3)
+AstronautMission.create!(astronaut: astro3, mission: mission3)
