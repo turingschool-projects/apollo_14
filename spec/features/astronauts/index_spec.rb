72b1192a0e9030a3a9d8a79efe6c73288c776629
Ely Hess <ely.hess@me.com>
2020-12-21 10:43:25 -0700

    Add do keywords

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -12,34 +12,34 @@ describe "As a visitor" do
       @mission2 = Mission.create!(title: "Mission to Moon", time_in_space: 1)
       @mission3 = Mission.create!(title: "Mission to Sun", time_in_space: 80)
 
-      @w = AstronautMission.create!(astronaut: astro1, mission: mission1)
-      @x = AstronautMission.create!(astronaut: astro1, mission: mission2)
-      @y = AstronautMission.create!(astronaut: astro2, mission: mission3)
-      @z = AstronautMission.create!(astronaut: astro3, mission: mission3)
+      @w = AstronautMission.create!(astronaut: @astro1, mission: @mission1)
+      @x = AstronautMission.create!(astronaut: @astro1, mission: @mission2)
+      @y = AstronautMission.create!(astronaut: @astro2, mission: @mission3)
+      @z = AstronautMission.create!(astronaut: @astro3, mission: @mission3)
     end
 
     it "I see a list of astronauts with the following info:" do
-      visit astronauts
+      visit astronauts_path
 
-      within("#astronaut-#{@astro1.id}")
+      within("#astronaut-#{@astro1.id}") do
         expect(page).to have_content(@astro1.name)
         expect(page).to have_content("Age: #{@astro1.age}")
         expect(page).to have_content("Job: #{@astro1.job}")
       end
 
-      within("#astronaut-#{@astro2.id}")
+      within("#astronaut-#{@astro2.id}") do
         expect(page).to have_content(@astro2.name)
         expect(page).to have_content("Age: #{@astro2.age}")
         expect(page).to have_content("Job: #{@astro2.job}")
       end
 
-      within("#astronaut-#{@astro3.id}")
+      within("#astronaut-#{@astro3.id}") do
         expect(page).to have_content(@astro3.name)
         expect(page).to have_content("Age: #{@astro3.age}")
         expect(page).to have_content("Job: #{@astro3.job}")
       end
 
-      within("#astronaut-#{@astro4.id}")
+      within("#astronaut-#{@astro4.id}") do
         expect(page).to have_content(@astro3.name)
         expect(page).to have_content("Age: #{@astro3.age}")
         expect(page).to have_content("Job: #{@astro3.job}")
