f0cfc07d779c0bdd6b2fa8e53d97c134f9eb06d2
Dani Coleman <danicoleman00@gmail.com>
2021-03-28 11:15:22 -0500

    Add method to get total space time
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,4 +3,8 @@ class Mission < ApplicationRecord
 
   has_many :astronaut_missions
   has_many :astronauts, through: :astronaut_missions
+
+  def self.total_time_in_space
+    sum(:time_in_space)
+  end
 end
