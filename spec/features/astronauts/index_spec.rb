6b3dfa9300523dbc413fb29db419d061ab6b1147
Dani Coleman <danicoleman00@gmail.com>
2021-03-28 11:13:35 -0500

    Add test to get astronauts total time in space
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Astronaut Index Page' do
     @astro3 = @astronaut_list.third
     @astro4 = @astronaut_list.fourth
     @astro5 = @astronaut_list.fifth
-    
+
     @mission1 = Mission.create!(title: "Zulu", time_in_space: 444)
     @mission2 = Mission.create!(title: "Moon", time_in_space: 8854)
     @mission3 = Mission.create!(title: "Sagittarius A", time_in_space: 9965)
@@ -27,7 +27,7 @@ RSpec.describe 'Astronaut Index Page' do
 
     expect(page).to have_css('.astronaut', count: 5)
     expect(page).to have_content("Apollo 14 Astronauts")
-    
+
     within(first('.astronaut')) do
       expect(page).to have_css('.name')
       expect(page).to have_css('.age')
@@ -39,17 +39,25 @@ RSpec.describe 'Astronaut Index Page' do
     visit astronauts_path
     expect(current_path).to eq('/astronauts')
     all_astros = Astronaut.all
-    average_age = all_astros.average_age 
+    average_age = all_astros.average_age
     expect(page).to have_content("Average age of all astronauts: #{average_age}")
   end
 
   it 'can see a list of an astronauts missions in alphabetical order' do
     visit astronauts_path
     expect(current_path).to eq(astronauts_path)
-    
-    within(first('.astronaut')) do 
+
+    within(first('.astronaut')) do
       expect(@mission2.title).to appear_before(@mission3.title)
       expect(@mission3.title).to appear_before(@mission1.title)
+    end
+  end
+
+  it "can see the total time in space for each astronaut" do
+    visit astronauts_path
+
+    within(first('.astronaut')) do
+      expect(page).to have_content("Total Time in Space: 19263")
     end
   end
 end
